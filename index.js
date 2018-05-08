1c47c1a7af723486e1b82448744d99c1e7f2a1fe
Choeles Tung <choelestung@gmail.com>
2018-05-08 00:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value)  {
   object[key] = value;
-  Object.assign({}, object, key, value);
+  Object.assign({}, object, key, value: 2);
   return object;
 }
