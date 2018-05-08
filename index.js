c4f5c62feb7d3bb7dc6b70a9561e095794154939
Choeles Tung <choelestung@gmail.com>
2018-05-08 00:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function destructivelyDeleteFromObjectByKey(object, key)  {
   return deletingObject;
 }
 function destructivelyDeleteFromObjectByKey(object, key)  {
-  var modObject = Object.assign({}, object key)
+  var modObject = Object.assign({}, object, key)
    return modObject;
 }
