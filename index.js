61328a33c10f254330d975bb0b21bb4160bea099
Choeles Tung <choelestung@gmail.com>
2018-05-08 00:29:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function deleteFromObjectByKey(object, key) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign ({}, object)
   delete newObj.key;
-  return newObj;
+  return object;
 }
